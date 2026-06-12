733231a06bb9f20337ca2032cd98e14fc3f592ff
satya <gudelasatya@gmail.com>
2026-06-12 15:35:03 +0530

    apptest.java updated

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,7 +1,7 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.顕sertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
